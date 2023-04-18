472b0b530278aee8b786ce864a89b4c4cdc329ee
Jovani Benavides <xavibenavides@yahoo.com>
2023-04-17 19:42:39 -0700

    FINISH AT HOME

--- COMP003B.Assignment4/Controllers/HomeController.cs
+++ COMP003B.Assignment4/Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace COMP003B.Assignment4.Controllers
         { 
             if (!ModelState.IsValid)
             {
-                return View(model);
+                return View("Contact",model);
             }
             return View(model);
         }
